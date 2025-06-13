09a192c9b7624f60cb1f5ba9e76fdabbfdf134cf
V Shree Ishitha <147683993+Ishita215@users.noreply.github.com>
2025-06-13 16:48:14 +0530

    Add files via upload

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,25 @@
+import { createContext, useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../firebase";
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true); 
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user || null);
+      setLoading(false); // 
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ currentUser, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
